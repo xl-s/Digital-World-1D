bc1c6f8969ab6eb83f3306ccdf346bfd6a73e5eb
xl-s <45271592+xl-s@users.noreply.github.com>
2019-04-07 10:40:01 +0800

    Update database.py

--- database.py
+++ database.py
@@ -18,23 +18,27 @@ class Database:
         firebase = pyrebase.initialize_app(config)
         self.db = firebase.database()
         
+
     def _transduce(self, pyredata):
         data = {}
         for dat in pyredata.each():
             data[dat.key()] = dat.val()
         return data
     
+
     def _getHash(self, data):
         h = hashlib.sha256()
         h.update(bytes(data, encoding='utf-8'))
         return h.hexdigest()
         
+
     def _push(self, directory, content):
         if hasattr(content, '__iter__'):
             self.db.child(directory).update(content)
         else:
             self.db.child(directory).set(content)
     
+
     def _pull(self, directory):
         data = self.db.child(directory).get()
         if data.each():
@@ -42,51 +46,72 @@ class Database:
         else:
             return data.val()
 
+
+    def _getOccupied(self):
+        return self.pullAPP('OCCUPIED')
+    
+
+    def _setOccupied(self, occupancy):
+        self.pushAPP(occupancy, 'OCCUPIED')
+
+
+    def _getBookings(self):
+        self.pushAPP(False, 'BOOKINGS/NEW')
+        return [booking for booking in self.pullAPP('BOOKINGS').values()]
+    
+
+    def _setBookings(self, bookings):
+        print('Please do not assign directly to the Bookings attribute. Instead, use the addBooking() method.')
+
+
+
     def pushRAW(self, content, directory=''):
         directory = '/RAWDATA/{}/{}'.format(self.roomID, directory.strip('/'))
         self._push(directory, content)
     
+
     def pushAPP(self, content, directory=''):
         directory = '/APPDATA/{}/{}'.format(self.roomID, directory.strip('/'))
         self._push(directory, content)
         
+
     def pushUSERS(self, content, directory=''):
         directory = '/USERS/{}'.format(directory.strip('/'))
         self._push(directory, content)
         
+
     def pullRAW(self, directory=''):
         directory = '/RAWDATA/{}/{}'.format(self.roomID, directory.strip('/'))
         return self._pull(directory)
     
+
     def pullAPP(self, directory=''):
         directory = '/APPDATA/{}/{}'.format(self.roomID, directory.strip('/'))
         return self._pull(directory)
     
+
     def pullUSERS(self, directory=''):
         directory = '/USERS/{}'.format(directory.strip('/'))
         return self._pull(directory)
-    
-    def _getBookings(self):
-        return self.pullAPP('BOOKINGS')
-    
-    def _setBookings(self, bookings):
-        print('Please do not assign directly to the Bookings attribute. Instead, use the addBooking() method.')
         
+
+
+    Bookings = property(_getBookings, _setBookings)
+    Occupied = property(_getOccupied, _setOccupied)
+
+
     def addBooking(self, booking):
         self.db.child('/APPDATA/{}/BOOKINGS'.format(self.roomID)).push(booking)
+        self.pushAPP(True, 'BOOKINGS/NEW')
     
-    def _getOccupied(self):
-        return self.pullAPP('OCCUPIED')
-    
-    def _setOccupied(self, occupancy):
-        self.pushAPP(occupancy, 'OCCUPIED')
-        
+
     def login(self, username, password):
         p = self._getHash(password)
         u = self._getHash(username)
         pw = self.pullUSERS(u)
         return True if p == pw else False
     
+
     def register(self, username, password):
         p = self._getHash(password)
         u = self._getHash(username)
@@ -95,9 +120,11 @@ class Database:
             return True
         else:
             return False
-      
-    Bookings = property(_getBookings, _setBookings)
-    Occupied = property(_getOccupied, _setOccupied)
+
+
+    def new(self):
+        return True if self.pullAPP('BOOKINGS/NEW') else False
+    
     
     def saveRAW(self, filename='RAWDATA'):
         filename += '.json'
